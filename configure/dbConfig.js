5bea8ac9a9259aef4e4a65462b6693f868d3811e
upendraabaris <upendra.abaris@gmail.com>
2025-07-09 16:41:46 +0530

    clock out fix

--- configure/dbConfig.js
+++ configure/dbConfig.js
@@ -9,9 +9,9 @@ const pool = new Pool({
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;

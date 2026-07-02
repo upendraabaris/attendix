9177c98c705f7dd9cf2ffeae67f851e437dc16b1
Rishi Singh Parihar <115362984+Rishisinghparihar@users.noreply.github.com>
2026-07-02 15:51:23 +0530

    enhanced workspace and master task

--- routes/masterTasksRoute.js
+++ routes/masterTasksRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createMasterTask, getMasterTasks } = require("../controllers/masterTaskCtrl");
+const { createMasterTask, getMasterTasks, updateMasterTask } = require("../controllers/masterTaskCtrl");
 const { authenticate } = require("../middleware/authMiddleware");
 
 router.post("/create", authenticate, createMasterTask);
 router.get("/workspace/:workspace_id", authenticate, getMasterTasks);
+router.put("/update/:id", authenticate, updateMasterTask);
 
 module.exports = router;

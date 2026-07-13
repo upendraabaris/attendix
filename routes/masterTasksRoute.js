36843baee1f0bf52a53285c6d92b861d206d8501
Rishi Singh Parihar <115362984+Rishisinghparihar@users.noreply.github.com>
2026-07-13 15:54:01 +0530

    adding the new workspace and enhancement

--- routes/masterTasksRoute.js
+++ routes/masterTasksRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createMasterTask, getMasterTasks, updateMasterTask } = require("../controllers/masterTaskCtrl");
+const { createMasterTask, getMasterTasks, updateMasterTask, getMyMasterTasks } = require("../controllers/masterTaskCtrl");
 const { authenticate } = require("../middleware/authMiddleware");
 
 router.post("/create", authenticate, createMasterTask);
 router.get("/workspace/:workspace_id", authenticate, getMasterTasks);
 router.put("/update/:id", authenticate, updateMasterTask);
+router.get("/my", authenticate, getMyMasterTasks);
 
 module.exports = router;
